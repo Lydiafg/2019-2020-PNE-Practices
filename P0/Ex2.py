12cd6565e7ec498388fc22483bf003e806792696
lydiafg <l.fernandezg.2019@alumnos.urjc.es>
2020-03-29 12:34:00 +0200

    seq_read_fasta() function_

--- P0/Ex2.py
+++ P0/Ex2.py
@@ -1,7 +1,11 @@
 from Seq0 import *
 
+folder = "../Session4/"
 filename = "U5.txt"
+file = folder + filename
 
+print("---- Exercise 2 ----")
 print("DNA file: ", filename)
+sequence = seq_read_fasta(file)
 print("The first 20 bases are: ")
-seq_read_fasta(filename)
+print(sequence[:20])
