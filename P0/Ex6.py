277bc211ddaaa5fc73e4a0754741aaa30a9e3a3f
lydiafg <l.fernandezg.2019@alumnos.urjc.es>
2020-04-07 13:32:34 +0200

    seq_reverse() function

---
+++ P0/Ex6.py
@@ -0,0 +1,16 @@
+from Seq0 import *
+
+folder = "../Session-04/"
+format_ = ".txt"
+genes = ["U5", "ADA", "FRAT1", "FXN", "U5"]
+bases = ['A', 'C', 'T', 'G']
+
+print("----- Exercise 6 ------")
+
+gene = genes[0]
+
+print(f"Gene {gene}:")
+sequence = seq_read_fasta(folder + genes[0] + format_)[:20]
+reverse = seq_reverse(sequence)
+print(f"Fragment : {sequence}")
+print(f"Reverse sequence : {reverse}")
