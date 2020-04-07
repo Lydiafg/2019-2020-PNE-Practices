72e492615d68fb510da967026aa98aefbc70deb8
lydiafg <l.fernandezg.2019@alumnos.urjc.es>
2020-04-07 13:31:51 +0200

    seq_count_base() function

---
+++ P0/Ex4.py
@@ -0,0 +1,16 @@
+from Seq0 import*
+
+folder = "../Session-04/"
+format_ = ".txt"
+genes = ["U5", "ADA", "FRAT1", "FXN", "U5"]
+bases = ['A', 'C', 'T', 'G']
+
+print("----- Exercise 4 ------")
+
+
+for gene in genes:
+    sequence = seq_read_fasta(folder + gene + format_)
+    print()
+    print(f"Gene {gene}:")
+    for base in bases:
+        print(f"  {base}: {seq_count_base(sequence)}")
