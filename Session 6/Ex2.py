53107bb2a49c6a988e7c53d1a3abf9e5d1e8e36a
lydiafg <l.fernandezg.2019@alumnos.urjc.es>
2020-04-08 14:03:36 +0200

    exercise 2

--- Session 6/Ex2.py
+++ Session 6/Ex2.py
@@ -7,11 +7,13 @@ class Seq:
     def __str__(self):
         return self.strbases
 
-    def print_seq(list_seq):
-        print(f"Sequence 0: {list_seq[0].len()}", f"{list_seq[0]}")
-        print(f"Sequence 1: {list_seq[1].len()}", f"{list_seq[1]}")
-        print(f"Sequence 2: {list_seq[2].len()}", f"{list_seq[2]}")
-        return
+
+def print_seq(list_seq):
+    print(f"Sequence 0: {list_seq[0].len()}", f"{list_seq[0]}")
+    print(f"Sequence 1: {list_seq[1].len()}", f"{list_seq[1]}")
+    print(f"Sequence 2: {list_seq[2].len()}", f"{list_seq[2]}")
+    return
+
 
 seq_list = [Seq("ACT"), Seq("GATA"), Seq("CAGATA")]
 print_seq(seq_list)
