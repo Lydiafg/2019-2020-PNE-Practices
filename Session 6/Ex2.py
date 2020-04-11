a0b43d3a0016260dcf5a2f6fd2fff8b0e88f7e68
lydiafg <l.fernandezg.2019@alumnos.urjc.es>
2020-04-11 12:43:33 +0200

    exercise 2

--- Session 6/Ex2.py
+++ Session 6/Ex2.py
@@ -1,12 +1,18 @@
 class Seq:
 
     def __init__(self, strbases):
-        self.strbases = strbases
+        bases = ["A", "C", "G", "T"]
+        for i in strbases:
+            if i not in bases:
+                print("ERROR!")
+                self.strbases = "ERROR"
+            else:
+                self.strbases = strbases
         print("New sequence created!")
+        return
 
     def __str__(self):
         return self.strbases
-
     def len(self):
         """Calculate the length of the sequence"""
         return len(self.strbases)
