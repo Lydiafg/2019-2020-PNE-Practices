a40a0fb61804efb35588e8d7509c016eb6f35598
lydiafg <l.fernandezg.2019@alumnos.urjc.es>
2020-04-12 13:28:23 +0200

    exercise 4 practice 1

--- P1/Ex4.py
+++ P1/Ex4.py
@@ -4,7 +4,7 @@ s1 = Seq()
 s2 = Seq("ACTGA")
 s3 = Seq("Invalid sequence")
 
-print("-----| Practice 1, Exercise 3 |------")
+print("-----| Practice 1, Exercise 4 |------")
 print()
 print(f"Sequence 1: (Length: {s1.len()})", f"{s1}")
 print(f"Sequence 2: (Length: {s2.len()})", f"{s2}")
