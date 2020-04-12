bea0e15fb7011102a7bd7313e7640246dade94b0
lydiafg <l.fernandezg.2019@alumnos.urjc.es>
2020-04-12 11:37:34 +0200

    exercise 4 practice 1

---
+++ P1/Ex4.py
@@ -0,0 +1,11 @@
+from Seq1 import Seq
+
+s1 = Seq()
+s2 = Seq("ACTGA")
+s3 = Seq("Invalid sequence")
+
+print("-----| Practice 1, Exercise 3 |------")
+print()
+print(f"Sequence 1: (Length: {s1.len()})", f"{s1}")
+print(f"Sequence 2: (Length: {s2.len()})", f"{s2}")
+print(f"Sequence 3: (Length: {s3.len()})", f"{s3}")
