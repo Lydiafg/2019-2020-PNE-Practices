8db3a684d366f41e4f9be8abb4c1da72a1209142
lydiafg <l.fernandezg.2019@alumnos.urjc.es>
2020-04-12 12:58:19 +0200

    exercise 7 practice 1

---
+++ P1/Ex7.py
@@ -0,0 +1,14 @@
+from Seq1 import Seq
+
+s1 = Seq()
+s2 = Seq("ACTGA")
+s3 = Seq("Invalid sequence")
+sequences = [s1, s2, s3]
+
+print("-----| Practice 1, Exercise 3 |------")
+print()
+
+for i, s in enumerate(sequences):
+    print(f"Sequence {i}: (Length: {s.len()})", f"{s}")
+    print(f" Bases : {s.count()}")
+    print(f"Rev: {s.reverse()}")
