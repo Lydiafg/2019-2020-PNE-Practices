775c807bc0df15b2dc54e0b3cab96abe291759f5
lydiafg <l.fernandezg.2019@alumnos.urjc.es>
2020-04-12 13:27:46 +0200

    exercise 6 practice 1

--- P1/Ex6.py
+++ P1/Ex6.py
@@ -5,7 +5,7 @@ s2 = Seq("ACTGA")
 s3 = Seq("Invalid sequence")
 sequences = [s1, s2, s3]
 
-print("-----| Practice 1, Exercise 3 |------")
+print("-----| Practice 1, Exercise 6 |------")
 print()
 
 for i, s in enumerate(sequences):
