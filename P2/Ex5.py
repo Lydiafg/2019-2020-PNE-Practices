f159307115ead918fc995a136ec62ec6c5fcdc64
lydiafg <l.fernandezg.2019@alumnos.urjc.es>
2020-04-16 12:50:14 +0200

    exercise 5 practice 2

--- P2/Ex5.py
+++ P2/Ex5.py
@@ -1,5 +1,5 @@
 from Client0 import Client
-from Seq1 import Seq
+from Seq1 import Seq  # se pueden importar funciones de diferentes clases a la vez, no hace falta volver a definirlas
 
 PRACTICE = 2
 EXERCISE = 5
@@ -12,11 +12,11 @@ folder = "../Session4/"
 filename = "U5"
 format_ = ".txt"
 
-complete_file = Seq().read_fasta(folder + filename + format_)
+complete_file = Seq().read_fasta(folder + filename + format_)  # importamos la función de la class Seq
 
 c = Client(IP, PORT)
 print(c)
 
 print("Sending a message to the server...")
 c.debug_talk("Sending U5 Gene to the server...")
-c.debug_talk(complete_file)
+c.debug_talk(str(complete_file))  # poner el tipo (str) pq sino no lo reconoce
