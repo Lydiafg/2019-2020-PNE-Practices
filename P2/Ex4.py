f94aa1a873b0aeb110636872eb9e1bcdc30bbd5f
lydiafg <l.fernandezg.2019@alumnos.urjc.es>
2020-04-16 12:38:41 +0200

    exercise 4 practice 2

---
+++ P2/Ex4.py
@@ -0,0 +1,16 @@
+from Client0 import Client
+
+PRACTICE = 2
+EXERCISE = 3
+
+print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
+
+IP = "192.168.1.52"  # tiene que ser el de mi ordenador
+PORT = 8080  # es el mismo pq depende de la apliación
+
+c = Client(IP, PORT)
+print(c)
+
+print("Sending a message to the server...")
+c.debug_talk("Message 1---")
+c.debug_talk("Testing!!!")
