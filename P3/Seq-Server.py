314b3fbf421226f8dca729af4027aaeb9886964a
lydiafg <l.fernandezg.2019@alumnos.urjc.es>
2020-04-17 14:07:35 +0200

    seq server

--- P3/Seq-Server.py
+++ P3/Seq-Server.py
@@ -1,23 +1,6 @@
-
+from Seq1 import Seq
 import socket
 import termcolor
-
-PORT = 8080
-IP = "127.0.0.1"
-
-# -- Step 1: create the socket
-ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# -- Optional: This is for avoiding the problem of Port already in use
-ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-
-# -- Step 2: Bind the socket to server's IP and PORT
-ls.bind((IP, PORT))
-
-# -- Step 3: Configure the socket for listening
-ls.listen()
-
-print("The server is configured!")
 
 seq0 = "ACCGTGC"
 seq1 = "TCGGAC"
@@ -25,81 +8,133 @@ seq2 = "GCATTGCA"
 seq3 = "CAGGTCA"
 seq4 = "CATTGTCGAGTGTGGCA"
 seq_list = [seq0, seq1, seq2, seq3, seq4]
-get_strings = ["GET 0", "GET 1", "GET 2", "GET 3", "GET 4"]
+
+
+def get_command(num):
+    return seq_list[num]
+
+
+def info_command(seq):
+    real_seq = Seq(seq)
+    length = real_seq.len()
+    count_A = real_seq.count_base("A")
+    count_C = real_seq.count_base("C")
+    count_G = real_seq.count_base("G")
+    count_T = real_seq.count_base("T")
+    per_A = 100 * (count_A / length)
+    per_C = 100 * (count_C / length)
+    per_G = 100 * (count_G / length)
+    per_T = 100 * (count_T / length)
+    response_info = f"""Total length {real_seq}: {length}
+                     A : {count_A} ({per_A} %)
+                     C : {count_C} ({per_C} %)
+                     G : {count_G} ({per_G} %)
+                     T : {count_T} ({per_T} %)"""
+    return response_info
+
+
+def comp_command(seq):
+    real_seq = Seq(seq)
+    complement_seq = real_seq.complement()
+    return complement_seq
+
+
+def rev_command(seq):
+    real_seq = Seq(seq)
+    reverse_seq = real_seq.reverse()
+    return reverse_seq
+
+
+def gene_command(seq):
+    folder = "../Session4/"
+    file = seq
+    format_ = ".txt"
+    filename = Seq().read_fasta(folder + file + format_)
+    return str(filename)
+
+
+PORT = 8080
+IP = "127.0.0.1"
+
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+ls.bind((IP, PORT))
+
+ls.listen()
+
+print("The server is configured!")
 
 
 while True:
-    # -- Waits for a client to connect
+
     print("Waiting for Clients to connect")
 
     try:
         (cs, client_ip_port) = ls.accept()
 
-    # -- Server stopped manually
     except KeyboardInterrupt:
         print("Server stopped by the user")
 
-        # -- Close the listenning socket
         ls.close()
 
-        # -- Exit!
         exit()
 
-    # -- Execute this part if there are no errors
     else:
         print("A client has connected to the server!")
 
-        # -- Read the message from the client
-        # -- The received message is in raw bytes
         msg_raw = cs.recv(2048)
 
-        # -- We decode it for converting it
-        # -- into a human-redeable string
         msg = msg_raw.decode()
 
-        if msg == "PING":
-            termcolor.cprint(f"{msg} command!", "green")
+        print(f"Message received: {msg}")
+
+        # ------ Process the command
+
+        # -- Remove the \n
+        lines = msg.split("\n")
+        line0 = lines[0].strip()
+
+        # -- Separate the line into command and argument
+
+        # -- Eliminate the blank spaces(then we have the message without elements we do not need --> command + argument)
+        real_msg = line0.split(' ')
+
+        # -- The first element is the command
+        command = real_msg[0]
+
+        # -- Get the first argument (which is the second element)
+        try:
+            argument = real_msg[1]
+        except IndexError:
+            argument = " "
+
+        # -- Response message
+        response = ""
+
+        if command == "PING":
+            termcolor.cprint("PING command!", 'green')
             response = "OK!"
-            cs.send(response.encode())
-            cs.close()
-
-        elif msg in get_strings:
-            print("GET")
-            if msg == "GET 0":
-                response = seq_list[0]
-                print(response)
-                cs.send(response.encode())
-                cs.close()
-            elif msg == "GET 1":
-                response = seq_list[1]
-                print(response)
-                cs.send(response.encode())
-                cs.close()
-            elif msg == "GET 2":
-                response = seq_list[2]
-                print(response)
-                cs.send(response.encode())
-                cs.close()
-            elif msg == "GET 3":
-                response = seq_list[3]
-                print(response)
-                cs.send(response.encode())
-                cs.close()
-            elif msg == "GET 4":
-                response = seq_list[4]
-                print(response)
-                cs.send(response.encode())
-                cs.close()
-
+        elif command == "GET":
+            termcolor.cprint("GET", 'green')
+            response = get_command(int(argument))
+        elif command == "INFO":
+            termcolor.cprint("INFO", 'green')
+            response = info_command(argument)
+        elif command == "COMP":
+            termcolor.cprint("COMP", 'green')
+            response = comp_command(argument)
+        elif command == "REV":
+            termcolor.cprint("REV", 'green')
+            response = rev_command(argument)
+        elif command == "GENE":
+            termcolor.cprint("GENE", 'green')
+            response = gene_command(argument)
         else:
-            # -- Print the received message
-            print(f"Message received: {msg}")
+            termcolor.cprint("Unknown command!!!", 'red')
+            response = "Unkwnown command"
 
-            # -- Send a response message to the client
-            response = "HELLO. I am the Happy Server :-)\n"
-
-            # -- The message has to be encoded into bytes
-            cs.send(response.encode())
-
-            # -- Close the data socket
-            cs.close()
+        print(response)
+        cs.send(response.encode())
+        cs.close()
