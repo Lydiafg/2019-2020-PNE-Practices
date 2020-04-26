8e61d2a0189d919ed1059dc0b455ef0cbe311a4c
lydiafg <l.fernandezg.2019@alumnos.urjc.es>
2020-04-26 20:14:33 +0200

    web server (html) practice 5

---
+++ P5/Bases2-webserver.py
@@ -0,0 +1,107 @@
+import http.server
+import socketserver
+import termcolor
+from pathlib import Path
+
+# Define the Server's port
+PORT = 8080
+
+# -- This is for preventing the error: "Port already in use"
+socketserver.TCPServer.allow_reuse_address = True
+
+
+# Class with our Handler. It is a called derived from BaseHTTPRequestHandler
+# It means that our class inheritates all his methods and properties
+class TestHandler(http.server.BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        """This method is called whenever the client invokes the GET method
+        in the HTTP protocol request"""
+
+        # Print the request line
+        termcolor.cprint(self.requestline, 'green')
+
+        req_line = self.requestline.split(' ')
+
+        method = req_line[0]
+        path = req_line[1]
+        path = path[1:]
+
+        # IN this simple server version:
+        # We are NOT processing the client's request
+        # It is a happy server: It always returns a message saying
+        # that everything is ok
+
+        # Message to send back to the client
+        contents = ""
+        status = 0
+        content_type = 'text/html'
+
+        if path == "":
+            contents = Path("index.html").read_text()
+            status = 200
+        elif path == "index.html":
+            contents = Path("index.html").read_text()
+            status = 200
+        elif path == "info/A.html":
+            contents = Path("A.html").read_text()
+            status = 200
+        elif path == "A.html":
+            contents = Path("A.html").read_text()
+            status = 200
+        elif path == "info/C.html":
+            contents = Path("C.html").read_text()
+            status = 200
+        elif path == "C.html":
+            contents = Path("C.html").read_text()
+            status = 200
+        elif path == "info/G.html":
+            contents = Path("G.html").read_text()
+            status = 200
+        elif path == "G.html":
+            contents = Path("G.html").read_text()
+            status = 200
+        elif path == "info/T.html":
+            contents = Path("T.html").read_text()
+            status = 200
+        elif path == "T.html":
+            contents = Path("T.html").read_text()
+            status = 200
+        else:
+            contents = Path("error.html").read_text()
+            status = 404
+        # Generating the response message
+        self.send_response(status)  # -- Status line: OK!
+
+        # Define the content-type header:
+        self.send_header('Content-Type', content_type)
+        self.send_header('Content-Length', len(contents.encode()))
+
+        # The header is finished
+        self.end_headers()
+
+        # Send the response message
+        self.wfile.write(contents.encode())
+
+        return
+
+
+# ------------------------
+# - Server MAIN program
+# ------------------------
+# -- Set the new handler
+Handler = TestHandler
+
+# -- Open the socket server
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+
+    print("Serving at PORT", PORT)
+
+    # -- Main loop: Attend the client. Whenever there is a new
+    # -- clint, the handler is called
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("")
+        print("Stoped by the user")
+        httpd.server_close()
