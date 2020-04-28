5cb8075e1cf713b9f9ce37d7dc3ce4f6ced26821
lydiafg <l.fernandezg.2019@alumnos.urjc.es>
2020-04-28 13:10:03 +0200

    server practice 6 exercise 2

--- P6/Seq2-server.py
+++ P6/Seq2-server.py
@@ -2,11 +2,14 @@ import http.server
 import socketserver
 import termcolor
 from pathlib import Path
+from Seq1 import Seq
 
 
 PORT = 8080
 
 socketserver.TCPServer.allow_reuse_address = True
+
+get_seq = ["ACCGT", "TACCG", "CGGTA", "GCCTA", "AAAGC"]
 
 class TestHandler(http.server.BaseHTTPRequestHandler):
 
@@ -23,16 +26,18 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
         arguments = path.split('?')
         verb = arguments[0]
 
+
         if path == "":
-            contents = Path("form-1.html").read_text()
+            contents = Path("form-2.html").read_text()
             status = 200
-        elif path == "/form-1.html":
-            contents = Path("form-1.html").read_text()
+        elif path == "/form-2.html":
+            contents = Path("form-2.html").read_text()
             status = 200
         elif verb == "/":
-            contents = Path('form-1.html').read_text()
+            contents = Path('form-2.html').read_text()
             status = 200
-        elif verb == "/echo":
+        elif verb == "/ping":
+
             contents = """
                         <!DOCTYPE html>
                         <html lang="en">
@@ -43,7 +48,28 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                         <body>
                         <h1>PING OK!</h1>
                         <h2>The Seq-2 Server is running...</h2>
-                        <a href="form-1.html">Main page</a>
+                        <a href="form-2.html">Main page</a>
+                        """
+            status = 200
+        elif verb == "/get":
+            pair = arguments[1]
+            pairs = pair.split('&')
+            option, number = pairs[0].split('=')
+            num = int(number)
+            sequence = get_seq[num]
+            contents = f"""
+                        <!DOCTYPE html>
+                        <html lang="en">
+                        <head>
+                            <meta charset="utf-8">
+                            <title>GET SEQUENCE</title>
+                        </head>
+                        <body>
+                        <h2>Sequence number {num}</h2>
+                        <p> {sequence} </p>
+                        <a href="form-2.html">Main page</a>
+                        </body>
+                        </html>
                         """
             status = 200
         else:
