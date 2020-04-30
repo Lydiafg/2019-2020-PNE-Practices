17d8fb6abc59f2ae12415cd9c9195c090c3f08a8
lydiafg <l.fernandezg.2019@alumnos.urjc.es>
2020-04-30 14:05:57 +0200

    INFO genes (exercise 4 practice 7)

---
+++ P7/EX04-gene-process.py
@@ -0,0 +1,84 @@
+import http.client
+import json
+import termcolor
+from Seq1 import Seq
+
+GENES = {
+     'FRAT1': 'ENSG00000165879',
+     'SRCAP': 'ENSG00000080603',
+     'ADA': 'ENSG00000196839',
+     'FXN': 'ENSG00000165060',
+     'RNU6_269P': 'ENSG00000212379',
+     'MIR633': 'ENSG00000207552',
+     'TTTY4C': 'ENSG00000228296',
+     'FGFR3': 'ENSG00000068078',
+     'KDR': 'ENSG00000128052',
+     'ANK2': 'ENSG00000145362',
+ }
+print()
+GENE = input("Write the gene name: ")
+
+SERVER = 'rest.ensembl.org'
+ENDPOINT = "/sequence/id/"
+PARAMETERS = "?content-type=application/json"
+
+REQ_LINE = ENDPOINT + GENES[GENE] + PARAMETERS
+URL = SERVER + REQ_LINE
+
+print()
+print(f"Server: {SERVER}")
+print(f"URL : {URL}")
+
+
+# Connect with the server
+conn = http.client.HTTPConnection(SERVER)
+
+# -- Send the request message, using the GET method. We are
+# -- requesting the main page (/)
+try:
+
+    conn.request("GET", REQ_LINE)
+except ConnectionRefusedError:
+    print("ERROR! Cannot connect to the Server")
+    exit()
+
+# -- Read the response message from the server
+r1 = conn.getresponse()
+
+# -- Print the status line
+print(f"Response received!: {r1.status} {r1.reason}\n")
+
+# -- Read the response's body
+data1 = r1.read().decode()
+
+# -- Create a variable with the data,
+# -- form the JSON received
+gene_info = json.loads(data1)
+
+print()
+termcolor.cprint("Gene :", "green", end=" ")
+print(f"{GENE}")
+termcolor.cprint("Description :", "green", end=" ")
+print(f"{gene_info['desc']}")
+
+sequence = Seq(gene_info['seq'])
+
+termcolor.cprint("Total length: ", "green", end=" ")
+print(f"{sequence.len()}")
+
+bases = ['A', 'C', 'G', 'T']
+
+for base in bases:
+    termcolor.cprint(base, "blue", end=" ")
+    print(f": {sequence.count_base(base)}", end=" ")
+    percentage_base = "{:.1f}".format(sequence.count_base(base)/sequence.len() * 100)
+    print(f"({percentage_base} %)")
+
+max = 0
+max_base = ""
+for base in bases:
+    if sequence.count_base(base) > int(max):
+        max = sequence.count_base(base)
+        max_base = base
+
+print("Most frequent base: ", {max_base})
